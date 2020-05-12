ed929a1d34be0708f78a62b690008d2257823d04
cyndisong28 <30833889+cyndisong28@users.noreply.github.com>
2020-05-12 05:44:00 +0000

    Saving progress for all questions.

--- invent-code-0/main.cpp
+++ invent-code-0/main.cpp
@@ -2,6 +2,31 @@
 #include <string>
 using namespace std;
 
+struct Driver {
+    string first_name;
+    string last_name;
+    int age;
+    string state;
+};
+
+Driver CreateDriver() {
+    Driver a;
+    cout << "Fist Name: ";
+    cin >> a.first_name;
+    cout << "Last Name: ";
+    cin >> a.last_name;
+    cout << "Age: ";
+    cin >> a.age;
+    cout << "State: ";
+    cin >> a.state;
+    return a;
+}
+
 int main() {
-  return 0;
+    Driver a = CreateDriver();
+    cout << "First Name: " << a.first_name << endl;
+    cout << "Last Name: " << a.last_name << endl;
+    cout << "Age: " << a.age << endl;
+    cout << "State: " << a.state << endl;
+
 }
